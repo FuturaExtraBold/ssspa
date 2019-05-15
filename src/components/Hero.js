d2383aa8ec3f71a0eeb80eed362f577b1e2ab1f5
Ben Hays <ben@kajabi.com>
2019-05-15 13:00:43 -0700

    Added hero images. Added some app JS.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,12 +2,13 @@ import React from "react";
 
 const Hero = () => (
   <section className="hero">
+    <div className="overlay hero__bg"></div>
     <div className="display-table">
       <div className="display-table__cell display-table__cell--middle">
         <div className="container hero__container">
           <div className="hero__text-container">
-            <p className="hero__niche">Interior Design</p>
-            <h1 className=" display--1 hero__title">Interior Designer Tom Delevan uses Kajabi to beautifully showcase his projects.</h1>
+            <p className="hero__niche">Backend Developer</p>
+            <h1 className=" display--1 hero__title">Backend Developer Andy Huynh uses Kajabi to beautifully showcase his projects.</h1>
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './stylesheets/index.scss';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./stylesheets/index.scss";
+import App from "./components/App";
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById("root")
 );
